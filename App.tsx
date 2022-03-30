4d62c7f5f7c7e1e2a2fc9ce72709f60a72657cb3
Vaggelis Tzironis <VTzironis@innovationsprint.eu>
2022-03-30 23:17:00 +0300

    Adding testing libraries

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up ToDoListApp to start working on your app!</Text>
@@ -9,6 +9,8 @@ export default function App() {
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
