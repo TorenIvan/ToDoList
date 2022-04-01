b03fb495c4d0e3a9fc03857646df2cf811d50c48
Vaggelis Tzironis <VTzironis@innovationsprint.eu>
2022-04-01 23:53:52 +0300

    Deleting comments from declaration file

--- src/assets/declarations.d.ts
+++ src/assets/declarations.d.ts
@@ -1,21 +1,3 @@
-// export { faviconIcon } from "./icons/favicon.png";
-
-// export { checkIcon } from "./icons/icon-check.svg";
-
-// export { crossIcon } from "./icons/icon-cross.svg";
-
-// export { moonIcon } from "./icons/icon-moon.svg";
-
-// export { sunIcon } from "./icons/icon-sun.svg";
-
-// export { darkDesktopBackgroundImage } from "./images/bg-desktop-dark.jpg";
-
-// export { lightDesktopBackgroundImage } from "./images/bg-desktop-light.jpg";
-
-// export { darkMobileBackgroundImage } from "./images/bg-mobile-dark.jpg";
-
-// export { lightMobileBackgroundImage } from "./images/bg-mobile-light.jpg";
-
 declare module "*.jpg" {
   import { ImageSourcePropType } from "react-native";
 
@@ -23,6 +5,7 @@ declare module "*.jpg" {
 
   export default content;
 }
+
 declare module "*.png" {
   import { ImageSourcePropType } from "react-native";
 
@@ -30,6 +13,7 @@ declare module "*.png" {
 
   export default content;
 }
+
 declare module "*.svg" {
   import React from "react";
   import { SvgProps } from "react-native-svg";
