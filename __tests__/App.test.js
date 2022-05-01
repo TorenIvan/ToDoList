37776c56c1bb112cab04b8af3aed8026e656ef23
Vaggelis Tzironis <VTzironis@innovationsprint.eu>
2022-05-01 19:12:12 +0300

    Initiating test vol.2

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,8 +1,11 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import App from "../App";
+import React from 'react';
+import renderer from 'react-test-renderer';
 
-test("render ToDoList properly", () => {
-  const { debug } = render(<App />);
-  debug();
+import App from '../App';
+
+describe('<App />', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
 });
