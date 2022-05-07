59135e8682fb4046d7e9c28c64f206bef7b878c7
Vaggelis Tzironis <VTzironis@innovationsprint.eu>
2022-05-07 16:29:40 +0300

    text input empty after submit test

--- __tests__/unit/createItem.test.js
+++ __tests__/unit/createItem.test.js
@@ -3,6 +3,7 @@ import { TextInput } from "react-native";
 import { create, act } from "react-test-renderer";
 import CreateItem from "../../src/containers/CreateItem";
 
+/* tree needs update to keep no changes after every test that acts and affects the next ones */
 const tree = create(<CreateItem />);
 
 describe("Create Item Container Testing", () => {
@@ -27,6 +28,8 @@ describe("Create Item Container Testing", () => {
   });
 
   it("should show the real-time text when textInput is written and checkbox is not pressed", () => {
+    const tree = create(<CreateItem />);
+
     const textInput = tree.root.findByType(TextInput).props;
     act(() => textInput.onChangeText("Some Text"));
 
@@ -34,7 +37,46 @@ describe("Create Item Container Testing", () => {
     expect(textInputUpdated.children).toEqual("Some Text");
   });
 
-  it("should show the real-time text with underline when textInput is written and checkbox is pressed", () => {});
+  it("should show the real-time text with underline when textInput is written and checkbox is pressed", () => {
+    const tree = create(<CreateItem />);
+
+    const textInput = tree.root.findByType(TextInput).props;
+    act(() => textInput.onChangeText("Some Text"));
+
+    expect(textInput.style).not.toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          textDecorationLine: "line-through",
+        }),
+      ])
+    );
+
+    const checkbox = tree.root.findByProps({ testId: "CheckBox" }).props;
+    act(() => checkbox.onRadioButtonPress());
+
+    const textInputUpdated = tree.root.findByType(TextInput).props;
+    expect(textInputUpdated.style).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          textDecorationLine: "line-through",
+        }),
+      ])
+    );
+  });
 
   test("text input blurs when keyboard hides", () => {});
+
+  test("text input becomes empty after submit", () => {
+    const onSubmit = () => {};
+    const tree = create(<CreateItem onSubmit={onSubmit} />);
+
+    const textInput = tree.root.findByType(TextInput).props;
+    act(() => textInput.onChangeText("Boring text"));
+
+    const textInputAfterChange = tree.root.findByType(TextInput).props;
+    act(() => textInputAfterChange.onSubmitEditing());
+
+    const textInputAfterSubmit = tree.root.findByType(TextInput).props;
+    expect(textInputAfterSubmit.children).toBe("");
+  });
 });
